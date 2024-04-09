26374227a50646858d78d97cfba38aeac856a891
asianParadissseee <asian.paradissseee@gmail.com>
2024-04-10 02:08:05 +0600

    add alias and config for styles

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -3,4 +3,17 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  source: {
+    alias: (alias) => {
+      return {
+        '@/atoms': './src/atoms',
+        '@/molecules': './src/molecules',
+        '@/organisms': './src/organisms',
+        '@/templates': './src/templates',
+        '@/pages': './src/pages',
+        '@/app': './src/app',
+        '@/commons': './src/commons'
+      };
+    }
+  }
 });
---
+++ src/app/app.jsx
@@ -0,0 +1,11 @@
+const App = () => {
+    return (
+        <div>
+            <h1 className="text-red-500">
+                hello
+            </h1>
+        </div>
+    );
+};
+
+export default App;
