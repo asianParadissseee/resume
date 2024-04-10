5a3fb4ccefe7583f00001e2a8136b3829fd1395f
asianParadissseee <asian.paradissseee@gmail.com>
2024-04-10 13:45:57 +0600

    add widgets and pages files

--- src/layouts/base-layout.jsx
+++ src/layouts/base-layout.jsx
@@ -1,10 +1,18 @@
+import {Fragment} from "react";
+import Navbar from "@/components/widgets/navbar";
+import Footer from "@/components/widgets/footer";
+import Sidebar from "@/components/widgets/sidebar";
 
 const BaseLayout = ({children}) => {
     return (
-        <div>
-            lox
-            {children}
-        </div>
+        <Fragment>
+            <Navbar/>
+            <main>
+                <Sidebar/>
+                {children}
+            </main>
+            <Footer/>
+        </Fragment>
     );
 };
 
