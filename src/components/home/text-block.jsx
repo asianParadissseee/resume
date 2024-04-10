87bb25a4370055ff5b4762b3878b2879ba64451a
asianParadissseee <asian.paradissseee@gmail.com>
2024-04-10 17:34:30 +0600

    add text animation

---
+++ src/components/home/text-block.jsx
@@ -0,0 +1,44 @@
+import {useEffect, useRef, useState} from "react";
+
+const TextBlock = () => {
+    const textRef = useRef(null);
+    const [scrollPosition, setScrollPosition] = useState(0);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            setScrollPosition(window.scrollY);
+        };
+        window.addEventListener("scroll", handleScroll);
+
+        return () => {
+            window.removeEventListener("scroll", handleScroll);
+        };
+    }, []);
+
+    const colorizedText = (text) => {
+        return text.split("").map((char, index) => {
+            let style = {
+                color: scrollPosition > 40 ? "white" : "gray",
+                transition: "color 5s",
+                transitionDelay: `${index * 10}ms`,
+            };
+            return (
+                <span key={index} style={style}>
+                        {char}
+                </span>
+            );
+        });
+    };
+
+    return (
+        <div className="mx-auto container px-10 flex justify-start sm:justify-end">
+            <h4 className="text-xl sm:text-3xl lg:text-5xl xl:text-7xl">
+                {colorizedText(
+                    "Helping brands to stand out in the digital era through a human centered design approach to turn ideas into interactive experiences."
+                )}
+            </h4>
+        </div>
+    );
+};
+
+export default TextBlock;
--- src/pages/home-page.jsx
+++ src/pages/home-page.jsx
@@ -1,18 +1,13 @@
 import BaseLayout from "@/layouts/base-layout";
 import Banner from "@/components/home/banner";
 import ScrollText from "@/components/home/scroll-text";
+import TextBlock from "@/components/home/text-block";
 
 const HomePage = () => {
     return (
         <BaseLayout>
             <Banner/>
-            <div className="mx-auto container px-10 flex justify-start sm:justify-end">
-                <h4 className="text-xl sm:text-3xl lg:text-5xl xl:text-7xl text-white">
-                    Helping brands to stand out in <br/> the digital era through a <br/> human centered
-                    design <br/> approach to turn ideas
-                    into <br/> interactive experiences."
-                </h4>
-            </div>
+            <TextBlock/>
             <ScrollText/>
         </BaseLayout>
     );
