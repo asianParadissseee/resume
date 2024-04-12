3995a5876c15470ede34c7578f21d6f98dbb3bf5
asianParadissseee <asian.paradissseee@gmail.com>
2024-04-12 14:00:17 +0600

    add hook use clipPath

---
+++ src/commons/hooks/use-clippath.jsx
@@ -0,0 +1,26 @@
+import {useInView} from "react-intersection-observer";
+
+export const useAnimatedClipPath = () => {
+    const { ref, inView } = useInView({ triggerOnce: true });
+
+    const variants = {
+        initial: {
+            y: 20,
+            opacity: 0,
+            clipPath: 'polygon(0 0, 0 0, 0 100%, 0% 100%)'
+        },
+        whileInView: {
+            y: 0,
+            opacity: 1,
+            clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'
+        }
+    };
+
+    const motionProps = {
+        initial: variants.initial,
+        animate: inView ? variants.whileInView : variants.initial,
+        transition: { duration: 2, ease: 'easeInOut' }
+    };
+
+    return { motionProps, ref };
+};
--- src/components/home/banner.jsx
+++ src/components/home/banner.jsx
@@ -1,52 +1,34 @@
 import TypewriterComponent from "typewriter-effect";
-import {motion} from "framer-motion";
-import BannerVideo from "@/commons/assets/videos/banner-video.mp4"
+import { motion } from "framer-motion";
+import BannerVideo from "@/commons/assets/videos/banner-video.mp4";
+import { useAnimatedClipPath } from "../../commons/hooks/use-clippath";
 
 const Banner = () => {
-
-    const variants = {
-        initial: {
-            clipPath: 'polygon(0 0, 0 0, 0 100%, 0% 100%)'
-        },
-        whileInView: {
-            clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'
-        }
-    };
+    const { motionProps, ref } = useAnimatedClipPath();
 
     return (
         <section id="home-banner" className="my-48 relative">
             <div className="container mx-auto px-10">
                 <div
-                    className="flex whitespace-pre text-3xl sm:text-7xl lg:text-8xl text-gray-300 font-bold flex-col gap-2">
-                    <motion.div
-                        initial={{
-                            y: 20,
-                            opacity: 0,
-                            clipPath: variants.initial.clipPath
-                        }}
-                        whileInView={{
-                            y: 0,
-                            opacity: 1,
-                            clipPath: variants.whileInView.clipPath
-                        }}
-                        viewport={{
-                            once: true
-                        }}
-                        transition={{duration: 2, ease: 'easeInOut'}}>
+                    className="flex whitespace-pre text-3xl sm:text-7xl lg:text-8xl text-gray-300 font-bold flex-col gap-2"
+                    ref={ref}
+                >
+                    <motion.div {...motionProps}>
                         <h1>
                             Hello everyone!
                         </h1>
                         <h2 className="ml-0 sm:ml-4">
-                            I'am Dadashi
+                            I'm Dadashi
                         </h2>
                         <h3 className="ml-0 sm:ml-8 text-xl sm:text-5xl lg:text-6xl gap-3 flex items-center">
-                            I'am
+                            I'm
                             <TypewriterComponent
                                 options={{
                                     strings: ["Software Engineer", "Designer", "Frontend Developer"],
                                     autoStart: true,
                                     loop: true
-                                }}/>
+                                }}
+                            />
                         </h3>
                     </motion.div>
                 </div>
