db4e7e7f327f13c831d0d593f13b5de73e110a86
asianParadissseee <asian.paradissseee@gmail.com>
2024-04-17 01:14:15 +0600

    add cards sticky scroll

--- src/components/ui/work-card.jsx
+++ src/components/ui/work-card.jsx
@@ -2,7 +2,7 @@ import React, {useRef} from "react";
 import {motion, useScroll, useTransform} from "framer-motion";
 
 
-const WorkCard = ({name, stack, bgColor, textColor, id, total, imgUrl}) => {
+const WorkCard = ({name, stack, bgColor, textColor, id, total, imgUrl,href}) => {
     const container = useRef(null)
     const {scrollYProgress} = useScroll({
         target: container,
@@ -13,14 +13,17 @@ const WorkCard = ({name, stack, bgColor, textColor, id, total, imgUrl}) => {
         <motion.div
             ref={container}
             style={{top: `${id * 3}rem`}}
-            className={`min-w-60 mt-20 sticky top-[${id * 3}rem] px-10 z-${total + id} text-center rounded-2xl py-10 h-svh ${textColor} ${bgColor}`}>
+            className={`min-w-60 mt-20 sticky px-10 z-${total + id} text-center rounded-2xl py-10 h-svh ${textColor} ${bgColor}`}>
             <p className="font-semibold text-3xl sm:text-6xl mb-10">{name.toUpperCase()}</p>
             <span className="font-normal text-sm sm:text-base">
         <b>Stack: </b>{stack}
       </span>
             <div className="flex justify-center items-center my-10">
-                <img src={imgUrl} className="hover:scale-110 transition duration-500 cursor-pointer"
-                     alt="вебсайт сделанный на определенной технологии с красивым дизайном" width={459} height={200}/>
+                <a href={href} target={"_blank"}>
+                    <img src={imgUrl} className="hover:scale-110 transition duration-500 cursor-pointer"
+                         alt="вебсайт сделанный на определенной технологии с красивым дизайном" width={459}
+                         height={200}/>
+                </a>
             </div>
         </motion.div>
     );
